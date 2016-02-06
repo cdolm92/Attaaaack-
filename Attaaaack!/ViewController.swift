1b88c3105c3e647bfe65a0388de08cf9ec354b69
cdolm92 <cdolm92@gmail.com>
2016-02-05 19:26:57 -0500

    attack button for player1 done

--- Attaaaack!/ViewController.swift
+++ Attaaaack!/ViewController.swift
@@ -9,17 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var printLbl: UILabel!
+    
+    @IBOutlet weak var player1HpLbl: UILabel!
+    
+    @IBOutlet weak var player2HpLbl: UILabel!
+    
+    @IBOutlet weak var player1Img: UIImageView!
 
+    @IBOutlet weak var player2Img: UIImageView!
+    
+    var player1: Player!
+    var player2: Player!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        player1 = Player(name: "PopOp23", hp: 110, attackPwr: 20)
+        
+        player2 = Player(name: "Knii46", hp: 110, attackPwr: 20)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func player1AttackTapped(sender: AnyObject) {
+       
+        if player2.attenptAttack(player1.attackPwr) {
+            printLbl.text = "\(player1.name) Attacked \(player2.name) for \(player1.attackPwr) HP"
+            player2HpLbl.text = "\(player2.hp) HP"
+        } else {
+            printLbl.text = "Attack was unsuccesful"
+        }
+        
+        if !player2.isAlive {
+            player2HpLbl.text = ""
+            printLbl.text = "\(player1.name) killed \(player2.name)"
+            player2Img.hidden = true
+        }
+        
     }
-
+  
+    @IBAction func player2AttackTapped(sender: AnyObject) {
+        
+        if
+        
+        
+    }
 
 }
 
