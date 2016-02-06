ddefe015f859b81d4c85e359d3bb444609879e8f
cdolm92 <cdolm92@gmail.com>
2016-02-05 20:28:40 -0500

    added start game function

--- Attaaaack!/ViewController.swift
+++ Attaaaack!/ViewController.swift
@@ -26,10 +26,40 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        startGame()
+       
+    }
+    
+    func startGame() {
+        
+        
         player1 = Player(name: "PopOp23", hp: 110, attackPwr: 20)
         
         player2 = Player(name: "Knii46", hp: 110, attackPwr: 20)
+        
+        player1HpLbl.text = "\(player1.hp) HP"
+        player2HpLbl.text = "\(player2.hp) HP"
+        
+        if player1Img.hidden == true{
+            player1Img.hidden = false
+            
+        }
+        
+        if player2Img.hidden == true {
+            player2Img.hidden = false
+        }
+        
     }
+    
+    func startGameAgain() {
+    
+        NSTimer.scheduledTimerWithTimeInterval(5.0, target: self, selector: "startGame", userInfo: nil, repeats: false)
+        
+       
+    
+        
+    }
+    
 
     @IBAction func player1AttackTapped(sender: AnyObject) {
        
@@ -44,13 +74,36 @@ class ViewController: UIViewController {
             player2HpLbl.text = ""
             printLbl.text = "\(player1.name) killed \(player2.name)"
             player2Img.hidden = true
+           
+            startGameAgain()
+            
         }
         
+       
+        
     }
+    
+    
   
     @IBAction func player2AttackTapped(sender: AnyObject) {
         
-        if
+        if player1.attenptAttack(player2.attackPwr) {
+            printLbl.text = "\(player2.name) attacked \(player1.name) for \(player2.attackPwr) HP"
+            player1HpLbl.text = "\(player1.hp) HP"
+        } else {
+            printLbl.text = "Attack was unsuccessful"
+        }
+        
+        if !player1.isAlive {
+            player1HpLbl.text = ""
+            printLbl.text = "\(player2.name) killed \(player1.name)"
+            player1Img.hidden = true
+            
+            startGameAgain()
+            
+        }
+        
+      
         
         
     }
